b15ee1c2907f40de92f5ab9ed9729d358d88bf26
ShubhamRam-cogoport <shubham.ram@cogoport.com>
2023-10-29 20:10:01 +0530

    implemented logout feature

--- packages/common/Navbar/index.js
+++ packages/common/Navbar/index.js
@@ -1,11 +1,25 @@
 import navigation_mapping from '@/packages/configuration/navigation-mapping'
 import styles from './styles.module.css'
 import Link from 'next/link'
+import { setCookie } from '@/packages/utils/cookie';
+import { useRouter } from 'next/router';
 
 const PUBLIC_NAV = ['login', 'signup']
 
 function Navbar({ src = '' }) {
-    const showNav = !PUBLIC_NAV.includes(src)
+    const showNav = !PUBLIC_NAV.includes(src);
+
+    const {push} = useRouter();
+
+    const onClickLogout = ()=>{
+        setCookie({
+            cookieName: "user_id",
+            cookieValue: "",
+            cookieExpiryDays: -1
+        });
+        
+        push('/login');
+    }
 
     return (
         <nav className={`${styles.flex_box} ${styles.container}`}>
@@ -26,8 +40,16 @@ function Navbar({ src = '' }) {
                         })}
                     </div>
 
-                    <div>
+                    <div className={`${styles.flex_box} ${styles.right_content}`}>
                         <Link href='/my-cart'>My Cart</Link>
+
+                        <button 
+                            type='button'
+                            className={styles.logout_btn}
+                            onClick={onClickLogout}
+                        >
+                            Logout
+                        </button>
                     </div>
                 </>
                 : null
